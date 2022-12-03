a793e131828142054d464fb2ddc20e3d381129ec
lpd-earlham <lorcan.pigott-dix@earlham.ac.uk>
2022-12-03 17:56:02 +0000

    First commit

---
+++ projects/slice/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    let s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    // s.clear();
+
+
+    println!("the first word is: {}", word);
+
+
+}
+
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return i;
+//         }
+//     }
+//     s.len()
+// }
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
